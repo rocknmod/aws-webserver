279900c43aa1b2c4d8081ef47a4438468df6ddba
rocknmod <rocknmod@gmail.com>
2022-09-16 17:47:23 +0000

    3 - Commit

--- tfm-workspace/modules/network/network.tf
+++ tfm-workspace/modules/network/network.tf
@@ -1,38 +1,10 @@
 # Terraform network.tf module
-# Set up alternate providers for network module
-terraform {
-  required_providers {
-    aws = {
-      source                = "hashicorp/aws"
-      version               = ">= 2.7.0"
-      configuration_aliases = [aws.sec]
-    }
-  }
-}
 
-###############
-#create the VPCs
-###############
-
-# Create VPC in default ap-southeast-2
-
-resource "aws_vpc" "vpc_syd0001" {
-  cidr_block           = var.VPC_cidrblock
-  enable_dns_support   = var.enable_dns_support
-  enable_dns_hostnames = var.enable_dns_hostnames
-  tags = { Name = "vpc_syd0001"
-  }
-}
-
-# Create VPC in alernate eu-west-2
-
-resource "aws_vpc" "vpc_ldn0001" {
-  provider             = aws.sec
-  cidr_block           = var.VPC_cidrblock
-  enable_dns_support   = var.enable_dns_support
-  enable_dns_hostnames = var.enable_dns_hostnames
-  tags = { Name = "vpc_ldn0001"
-  }
-}
+#associating route table with the subnet
+resource "aws_route_table_association" "Public_Subnet_RT_Association" {
+	subnet_id = aws_subnet.public_subnet.id
+	route_table_id = aws_route_table.Public_RT.id
+} # end of association resource
 
 #end of VPC resource
+
