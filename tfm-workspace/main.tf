3f44fa8dea2b5e5aedff6d9c6558ff0c59d7bb17
rocknmod <rocknmod@gmail.com>
2022-09-16 17:53:09 +0000

    main.tf

--- tfm-workspace/main.tf
+++ tfm-workspace/main.tf
@@ -1,20 +1,15 @@
-# Terraform main file defines providers on 2 regions and modules
 # calling module for deploying network and ec2 instance
 
 provider "aws" {
-  profile = var.profile
-  region  = var.region-master
-  alias   = "region-master"
-}
-provider "aws" {
-  profile = var.profile
-  region  = var.region-worker
-  alias   = "region-worker"
+	region = var.region
 }
 
 module "network" {
-  source = "./modules/network"
-  providers = {
-    aws.sec = aws.region-worker
-  }
+	source = "./modules/network"
+}
+
+module "ec2instance" {
+	source = "./modules/compute"
+	vpc_subnet_id = module.network.subnet_id
+	ec2_security_group_id = module.network.security_group_id
 }
